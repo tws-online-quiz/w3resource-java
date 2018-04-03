3a2d25eb4d9588755296e290da0d60b2e7f2de1e
Pei Zhang <pezhang@thoughtworks.com>
2018-04-03 11:49:11 +0800

    modify answer script

--- script.sh
+++ script.sh
@@ -4,27 +4,34 @@ DataTypesExercisesCount=$(find ./ -type f -name "DataTypesExercises-*.java" | wc
 DateTimeCount=$(find ./ -type f -name "DateTime-*.java" | wc -l)
 NumbersCount=$(find ./ -type f -name "Numbers-*.java" | wc -l)
 InputOutputFileSystemCount=$(find ./ -type f -name "InputOutputFileSystem-*.java" | wc -l)
+AllJavaFileCount=$(find ./ -type f -name "*.java" | wc -l)
+isAvailable=0
 
-if [ ! ${DataTypesExercisesCount} -eq 10 ]; then
+if [ ${DataTypesExercisesCount} -lt 10 ]; then
     echo "Data Types Exercises 练习数量不满10道!";
-    exit 1;
+    isAvailable=1
 fi
 
-if [ ! ${DateTimeCount} -eq 37 ]; then
+if [ ${DateTimeCount} -lt 37 ]; then
     echo "Date Time 练习数量不满37道!";
-    exit 1;
+    isAvailable=1
 fi
 
 
-if [ ! ${NumbersCount} -eq 28 ]; then
+if [ ${NumbersCount} -lt 28 ]; then
     echo "Numbers 练习数量不满28道!";
-    exit 1;
+    isAvailable=1
 fi
 
 
-if [ ! ${InputOutputFileSystemCount} -eq 18 ]; then
+if [ ${InputOutputFileSystemCount} -lt 18 ]; then
     echo "Input-Output-File-System 练习数量不满18道!";
-    exit 1;
+    isAvailable=1
+fi
+
+if [ ${isAvailable} -eq 1 -a  ${AllJavaFileCount} -lt 93 ]; then
+    echo "练习题总数量不足93道!"
+    exit 1
 fi
 
 
